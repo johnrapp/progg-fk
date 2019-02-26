9ffddef68d868ebe26d146415b6a5c2b7d1ea966
John Rapp <johnfarnes@hotmail.com>
2019-02-26 22:05:21 +0100

    Cont. labb 4

--- lab4/src/mountain/RandomUtilities.java
+++ lab4/src/mountain/RandomUtilities.java
@@ -7,4 +7,9 @@ public class RandomUtilities {
 			t = -t;
 		return t;
 	}
+
+	public static Point offset(Point p, double dev, int order) {
+		double offset = randFunc(dev) / Math.pow(2, order);
+		return new Point(p.getX(), (int) (p.getY() + offset));
+	}
 }
